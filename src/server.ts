8fd03960acfc91f35c811c4898fa3e877f6a633d
Pedro Fortini <fortini_ooi@hotmail.com>
2025-07-11 11:16:45 -0300

    register new routes

--- src/server.ts
+++ src/server.ts
@@ -1,26 +1,32 @@
-import { fastifyCors } from "@fastify/cors";
-import { fastify } from "fastify";
+import { fastifyCors } from '@fastify/cors'
+import { fastify } from 'fastify'
 import {
   serializerCompiler,
   validatorCompiler,
-  type ZodTypeProvider,
-} from "fastify-type-provider-zod";
-import { env } from "./env.ts";
-import { getRoomsRoute } from "./http/routes/get-room.ts";
+  type ZodTypeProvider
+} from 'fastify-type-provider-zod'
+import { env } from './env.ts'
+import { createQuestionRoute } from './http/routes/create-question.ts'
+import { createRoomRoute } from './http/routes/create-room.ts'
+import { getRoomsRoute } from './http/routes/get-room.ts'
+import { getRoomQuestionsRoute } from './http/routes/get-room-questions.ts'
 
-const app = fastify().withTypeProvider<ZodTypeProvider>();
+const app = fastify().withTypeProvider<ZodTypeProvider>()
 
 app.register(fastifyCors, {
-  origin: ["http://localhost:5173", "http://127.0.0.1:5173"],
-});
+  origin: ['http://localhost:5173', 'http://127.0.0.1:5173']
+})
 
-app.setSerializerCompiler(serializerCompiler);
-app.setValidatorCompiler(validatorCompiler);
+app.setSerializerCompiler(serializerCompiler)
+app.setValidatorCompiler(validatorCompiler)
 
-app.get("/health", () => {
-  return { status: "OK" };
-});
+app.get('/health', () => {
+  return { status: 'OK' }
+})
 
-app.register(getRoomsRoute);
+app.register(getRoomsRoute)
+app.register(createRoomRoute)
+app.register(getRoomQuestionsRoute)
+app.register(createQuestionRoute)
 
-app.listen({ port: env.PORT });
+app.listen({ port: env.PORT })
