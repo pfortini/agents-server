5e92d9a53473ab574b590d9ea5c09954fd7ba153
Pedro Fortini <fortini_ooi@hotmail.com>
2025-07-11 11:16:52 -0300

    update seed

--- src/db/seed.ts
+++ src/db/seed.ts
@@ -1,8 +1,8 @@
-import { reset, seed } from "drizzle-seed";
-import { db, sql } from "./connection.ts";
-import { schema } from "./schema/index.ts";
+import { reset, seed } from 'drizzle-seed'
+import { db, sql } from './connection.ts'
+import { schema } from './schema/index.ts'
 
-await reset(db, schema);
+await reset(db, schema)
 
 await seed(db, schema).refine((f) => {
   return {
@@ -10,10 +10,13 @@ await seed(db, schema).refine((f) => {
       count: 20,
       columns: {
         name: f.companyName(),
-        description: f.loremIpsum(),
+        description: f.loremIpsum()
       },
-    },
-  };
-});
+      with: {
+        questions: 5
+      }
+    }
+  }
+})
 
-await sql.end();
+await sql.end()
